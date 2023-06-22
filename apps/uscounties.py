08b1898afb33e85cef42f1da4b69311c79a4ef6e
sim-arora <simran.arora@jacobs.com>
2023-06-22 10:24:31 -0400

    Update uscounties.py

--- apps/uscounties.py
+++ apps/uscounties.py
@@ -7,8 +7,9 @@ def app():
     st.title("US Counties")
 
     filepath = "https://github.com/sim-arora/streamlit-apps/blob/main/data/georef-united-states-of-america-county.geojson"
-    m = leafmap.Map(tiles="stamentoner")
+    m = leafmap.Map(center=[40, -100], zoom=4)
     m.add_geojson(
-        in_geojson=filepath
+        in_geojson=filepath,
+        info_mode="coty_name_long"
     )
     m.to_streamlit(height=700)
