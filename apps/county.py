8f193226ee3d95f876e48f901f5c6116fabc5bb4
sim-arora <simran.arora@jacobs.com>
2023-06-25 19:37:08 -0400

    added county selector

--- apps/county.py
+++ apps/county.py
@@ -15,7 +15,17 @@ def draw_from_file(filepath):
         coordinates = list(polygons.exterior.coords)
         folium.Polygon(locations=coordinates).add_to(m)
 
+    drawn_line = st.map.draw_polyline()
+    if drawn_line is not None:
+        line = LineString(drawn_line)
+
+        selected_counties = gdf[gdf.intersects(line)]
+
+        st.subheader("Selected Counties")
+        st.write(selected_counties)
+
     folium_static(m)
+
 
 file_path = "https://github.com/sim-arora/streamlit-apps/blob/main/data/georef-united-states-of-america-county.geojson"
 
